6bf5ddb5f7ecacb9a5f60c48576bc6c9d1307b10
ratul <ratul0947@gmail.com>
2023-02-15 17:40:00 +0600

    cors updated

--- index.js
+++ index.js
@@ -30,7 +30,12 @@ app.get("/", (req, res) => {
 	res.send("okay");
 });
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+	})
+);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/user", userRoutes);
