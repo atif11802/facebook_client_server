91b5212e5993b69bb261465ceb443ff727b7939e
ratul <ratul0947@gmail.com>
2023-02-15 18:12:05 +0600

    cors option updated

--- index.js
+++ index.js
@@ -30,12 +30,21 @@ app.get("/", (req, res) => {
 	res.send("okay");
 });
 
-app.use(
-	cors({
-		origin: "*",
-		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-	})
-);
+var whitelist = [
+	"https://facebook-clone-client-izd2xacib-atif11802.vercel.app/",
+	"http://localhost:3000/",
+];
+var corsOptions = {
+	origin: function (origin, callback) {
+		if (whitelist.indexOf(origin) !== -1) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+};
+
+app.use(cors(corsOptions));
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/user", userRoutes);
